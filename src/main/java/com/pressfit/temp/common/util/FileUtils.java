727832289509d41c3c5383b77a099d4125bd3d6b
DESKTOP-LQP8LML\bit <phu1210@naver.com>
2017-03-24 09:32:14 +0900

    3.24(1)

--- src/main/java/com/pressfit/temp/common/util/FileUtils.java
+++ src/main/java/com/pressfit/temp/common/util/FileUtils.java
@@ -15,7 +15,7 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Component("fileUtils")
 public class FileUtils {
-	private static final String filePath = "C:\\dev\\file\\";
+	private static final String filePath = "/usr/local/share/mouseIMG";
 	
 	public List<Map<String,Object>> parseInsertFileInfo(Map<String,Object> map, HttpServletRequest request) throws Exception{
 		MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest)request;
